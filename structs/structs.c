19fd17f1a3ad93a46899550f3954e3fd9acd27c5
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-05-29 19:59:37 -0500

    structs

--- structs/structs.c
+++ structs/structs.c
@@ -47,7 +47,11 @@
 // 	- the C programming language doesn't have Objects like JS or Python.
 // 	- BUT we can use structs to model pieces of data that need multiple variables of different types to represent them!
 
-
+// Remember!:
+// gcc -o structs structs.c
+// ./structs
+// You can add -g -Wall for debugging purposes 
+// gcc -Wall -Wextra -o structs structs.c
 
 /* 
     Define the Person struct by specifying the fields that make up the
@@ -55,11 +59,11 @@
     Person should have the fields `name`, `age`, `height`, and `weight`.
 */
 
-typedef struct Person {
-    char name;
-    int age;
-    int height;
-    int weight;
+typedef struct Person { // Person struct (structure) to group related pieces of data, regardless of the type using typedef when creating it so make it easier to call it later in code.
+    char *name; // Name field char for a string
+    int age; // Age field int for integer 
+    int height; // Height field int for integer 
+    int weight; // Weight field int for integer 
 } Person;
 
 /*
@@ -70,9 +74,16 @@ typedef struct Person {
     When setting the `name` field, use your previously-built string_dup
     function, which handles the allocation of memory for the new string.
 */
-Person *createPerson(char *name, int age, int height, int weight)
+Person *createPerson(char *name, int age, int height, int weight) //Creates an instance of the Person struct that receives all the relevant pieces of data associated with a Person instance.
 {
+    
+    Person *person = malloc(sizeof(Person)); // Allocates the appropriate amount of memory to hold an instance of the Person struct
+    person->name = string_dup(name); // When setting the `name` field, use your previously-built string_dup function, which handles the allocation of memory for the new string.
+    person->age = age; // Use a dot notation when you have variable inside the struct and you use the arrow when you have a pointer to the struct
+    person->height = height;
+    person->weight = weight;
 
+    return person;
 }
 
 /*
@@ -81,7 +92,8 @@ Person *createPerson(char *name, int age, int height, int weight)
 */
 void destroyPerson(Person *who)
 {
-
+    free(who->name); // Frees up the memory that holds the Person's name.
+    free(who); // Frees up the memory that holds the Person's instance.
 }
 
 #ifndef TESTING
